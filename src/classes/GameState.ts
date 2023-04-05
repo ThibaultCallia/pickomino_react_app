d93a38a25eef0d66427d80f774ecab0bd614bf21
Thibault Calliauw <calliauw.t@gmail.com>
2023-04-05 16:49:21 +0200

    Added default tile set

--- src/classes/GameState.ts
+++ src/classes/GameState.ts
@@ -1,6 +1,7 @@
 import { nanoid } from "nanoid";
 import Player from "./Player";
 import { createUniqueNameArray } from "../helpers";
+import  defaultTilesSet  from "./tiles.const";
 
 interface Tile{
     value: number;
@@ -16,24 +17,7 @@ class GameState{
 
     constructor(numOfPlayers: number){
         this.playerArray = Array.from({ length: numOfPlayers }, () => new Player());
-        this.tilesArray = [
-            { value: 21, points: 1 },
-      { value: 22, points: 1 },
-      { value: 23, points: 1 },
-      { value: 24, points: 1 },
-      { value: 25, points: 2 },
-      { value: 26, points: 2 },
-      { value: 27, points: 2 },
-      { value: 28, points: 2 },
-      { value: 29, points: 3 },
-      { value: 30, points: 3 },
-      { value: 31, points: 3 },
-      { value: 32, points: 3 },
-      { value: 33, points: 4 },
-      { value: 34, points: 4 },
-      { value: 35, points: 4 },
-      { value: 36, points: 4 },
-        ];
+        this.tilesArray = defaultTilesSet;
         this.createPlayerNames(numOfPlayers);
     }
     createPlayerNames(numOfPlayers: number){
