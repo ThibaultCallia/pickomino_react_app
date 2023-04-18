524677940a9383b7787e6cb64fb4c875983758f7
Thibault Calliauw <calliauw.t@gmail.com>
2023-04-18 09:53:40 +0200

    waiting for players component

---
+++ src/components/WaitingForPlayers/WaitingForPlayers.tsx
@@ -0,0 +1,23 @@
+import { Text, Box } from "@chakra-ui/react"
+import { useSelector } from "react-redux"
+import { RootState } from "../../store"
+
+
+function WaitingForPlayers() {
+    const playersJoined = useSelector(
+        (state: RootState) => state.room.playersJoined
+    )
+    const maxPlayers = useSelector((state: RootState) => state.room.maxPlayers)
+    return (
+        <Box>
+                        <Text fontSize="xl" fontWeight="bold">
+                            Waiting for players...
+                        </Text>
+                        <Text fontSize="lg">
+                            {playersJoined}/{maxPlayers} players joined
+                        </Text>
+                    </Box>
+    )
+}
+
+export default WaitingForPlayers
