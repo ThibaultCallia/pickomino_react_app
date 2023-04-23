6b40fe71d03fce25d59ef4dc05b3ed326f2d83eb
Thibault Calliauw <calliauw.t@gmail.com>
2023-04-23 10:50:23 +0200

    Autoclose (with cleanup)

--- src/components/EndTurnModal/EndTurnModal.tsx
+++ src/components/EndTurnModal/EndTurnModal.tsx
@@ -1,43 +1,72 @@
+import React, { useState, useEffect } from "react";
 import {
-    Button,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay,
-} from "@chakra-ui/react"
-import { EndTurnModalProps } from "./"
+    Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
+import { EndTurnModalProps } from "./";
 
 const EndTurnModal: React.FC<EndTurnModalProps> = ({
-    isOpen,
-    onClose,
-    title,
-    children,
+  isOpen,
+  onClose,
+  title,
+  children,
 }) => {
-    return (
-        <Modal
-            isCentered={false}
-            closeOnOverlayClick={false}
-            isOpen={isOpen}
-            onClose={onClose}
-            scrollBehavior="inside"
-        >
-            <ModalOverlay bg="blackAlpha.500" />
-            <ModalContent>
-                <ModalHeader>{title}</ModalHeader>
+  const [countdown, setCountdown] = useState(10);
 
-                <ModalBody>{children}</ModalBody>
+  useEffect(() => {
+    if (isOpen) {
+      setCountdown(10);
+      const timer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
 
-                <ModalFooter>
-                    <Button colorScheme="yellow" mr={3} onClick={onClose}>
-                        End Turn
-                    </Button>
-                </ModalFooter>
-            </ModalContent>
-        </Modal>
-    )
-}
+      const closeTimer = setTimeout(() => {
+        onClose();
+      }, 10000);
 
-export default EndTurnModal
+      return () => {
+        clearInterval(timer);
+        clearTimeout(closeTimer);
+      };
+    }
+  }, [isOpen, onClose]);
+
+  return (
+    <Modal
+      isCentered={false}
+      closeOnOverlayClick={false}
+      isOpen={isOpen}
+      onClose={onClose}
+      scrollBehavior="inside"
+      
+    >
+      <ModalOverlay bg="blackAlpha.500" />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+
+        <ModalBody>{children}</ModalBody>
+        
+        <Box p={2} position={"absolute"} right={2} top={2}>
+            <Text fontSize={"xl"} fontWeight={"bold"}>{countdown}</Text>
+        </Box>
+
+        <ModalFooter>
+          
+          <Button colorScheme="yellow" mr={3} onClick={onClose}>
+            End Turn
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default EndTurnModal;
